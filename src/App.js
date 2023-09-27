55dcb71dd4ef09ed11befcb49690ec6ec1f33969
Denis Semenov <denissemenov@Deniss-Air.attlocal.net>
2023-09-27 15:48:56 -0700

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { data } from "./data";
+import "./App.css";
 
 function App() {
+  const [gifts, setGifts] = useState(data);
+
+  const removeGift = (id) => {
+    let newGifts = gifts.filter((gift) => gift.id !== id);
+    setGifts(newGifts);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <div className="container">
+        <h1>Christmas wish list</h1>
+        <h2>You have {gifts.length} items</h2>
+      </div>
+      {gifts.map((element) => {
+        const { id, gift, image } = element;
+
+        return (
+          <div key={id}>
+            <div className="container">
+              <h3>
+                {id} - {gift}
+              </h3>
+            </div>
+
+            <div className="container">
+              <img src={image} width="300px" height="350px" alt={gift} />
+            </div>
+
+            <div className="container">
+              <button onClick={() => removeGift(id)}>remove</button>
+            </div>
+          </div>
+        );
+      })}
+      <div className="delete">
+        <button onClick={() => setGifts([])}>delete all</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,69 @@
+export const data = [
+  {
+    id: 1,
+
+    gift: "Box of candies",
+
+    image:
+      "https://plus.unsplash.com/premium_photo-1671730770665-28da5f1d6dcd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2FuZHklMjBib3h8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    id: 2,
+    gift: "Romantic trip to Paris",
+    image:
+      "https://images.unsplash.com/photo-1522093007474-d86e9bf7ba6f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Zmx5JTIwdGlja2V0JTIwcGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    id: 3,
+
+    gift: "Earrings",
+
+    image:
+      "https://images.unsplash.com/photo-1617038220319-276d3cfab638?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8amV3ZWxyeXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    id: 4,
+
+    gift: "High heel shoes",
+
+    image:
+      "https://images.unsplash.com/photo-1543163521-1bf539c55dd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGlnaCUyMGhlZWwlMjBzaG9lc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    id: 5,
+
+    gift: "Bouquet Rose",
+
+    image:
+      "https://images.unsplash.com/photo-1497276236755-0f85ba99a126?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Ym91cXVldCUyMHJvc2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    id: 6,
+    gift: "Romantic dinner",
+    image:
+      "https://images.unsplash.com/photo-1569929233287-f0565228c4d4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cm9tYW50aWMlMjBkaW5uZXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    id: 7,
+
+    gift: "Iphone",
+
+    image:
+      "https://images.unsplash.com/photo-1616348436168-de43ad0db179?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGlwaG9uZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    id: 8,
+
+    gift: "Puppy dog",
+
+    image:
+      "https://images.unsplash.com/photo-1620189507187-1ecc7e2e9cff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHVwcHklMjBkb2d8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+  },
+];
